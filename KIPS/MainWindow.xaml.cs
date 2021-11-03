823f29cf4baaa500fa0aef16cbbfaf7a29955fc2
Dung Nguyen Huu <nh.dzung95@gmail.com>
2021-11-03 15:09:41 +0700

    First commit

---
+++ KIPS/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using NationalInstruments;
+using NationalInstruments.Analysis;
+using NationalInstruments.Analysis.Conversion;
+using NationalInstruments.Analysis.Dsp;
+using NationalInstruments.Analysis.Dsp.Filters;
+using NationalInstruments.Analysis.Math;
+using NationalInstruments.Analysis.Monitoring;
+using NationalInstruments.Analysis.SignalGeneration;
+using NationalInstruments.Analysis.SpectralMeasurements;
+using NationalInstruments.Controls;
+using NationalInstruments.Controls.Rendering;
+using NationalInstruments.NetworkVariable;
+using NationalInstruments.NetworkVariable.WindowsForms;
+using NationalInstruments.Tdms;
+using NationalInstruments.UI;
+using NationalInstruments.UI.WindowsForms;
+using NationalInstruments.ModularInstruments.NIScope;
+using NationalInstruments.ModularInstruments.NIDCPower;
+using NationalInstruments.ModularInstruments.NIDmm;
+using NationalInstruments.ModularInstruments.NIFgen;
+using NationalInstruments.ModularInstruments.NISwitch;
+using NationalInstruments.Visa;
+using NationalInstruments.DAQmx;
+using NationalInstruments.ModularInstruments;
+using NationalInstruments.ModularInstruments.SystemServices.DeviceServices;
+using NationalInstruments.ModularInstruments.SystemServices.TimingServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KIPS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
